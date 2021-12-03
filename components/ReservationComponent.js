91f3e7c567eac9814a485df29d7209970b967111
ErickG <erick818@gmail.com>
2021-12-02 18:05:06 -0800

    "Local Notifications"

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import {
-    Text, View, ScrollView, StyleSheet,
-    Picker, Switch, Button, Modal, Alert
-} from 'react-native';
+import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable'
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -42,8 +40,11 @@ class Reservation extends Component {
                     onPress: () => this.resetForm()
                 },
                 {
-                    test: 'OK',
-                    onPress: () => this.resetForm()
+                    text: 'OK',
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                        this.resetForm();
+                    }
                 }
             ],
             { cancelable: false }
@@ -60,16 +61,31 @@ class Reservation extends Component {
         });
     }
 
-    resetForm() {
-        this.setState({
-            campers: 1,
-            hikeIn: false,
-            date: new Date(),
-            showCalendar: false,
-            // showModal: false
-        });
-    }
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
 
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    }
 
     render() {
         return (
